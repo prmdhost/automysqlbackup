fd2d65eff0cbb8db72b4f150f45368f3a3930843
gm <markus.gerhold@coilinnovation.at>
2018-01-17 09:05:11 +0100

    add --ssl-mode for newer versions of mysql-server

--- install.sh
+++ install.sh
@@ -410,7 +410,7 @@ parse_config_file () {
 echo "### Checking archive files for existence, readability and integrity."
 echo
 
-precheck_files=( automysqlbackup DC318A9EA5C83D8D775D0D77BB1582D6
+precheck_files=( automysqlbackup 8E0149DA4D9C747F44710A9F592F4E5D
 automysqlbackup.conf 2bf3f76c7e0fb85888feb6930082202e
 README b17740fcd3a5f8579b907a42249a83cd
 LICENSE 39bba7d2cf0ba1036f2a6e2be52fe3f0
